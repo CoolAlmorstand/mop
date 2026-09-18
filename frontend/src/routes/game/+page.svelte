9bd4519caa1aec2446abf40b1102807a21fcdb23
Timothy Dexter <loinoobgaming33373@gmail.com>
2026-09-18 09:51:38 +0800

    setup game-page with pixijs

---
+++ frontend/src/routes/game/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { createPixiApp } from "$lib/pixi-js/init";
+  import { onMount } from "svelte";
+
+  let divContainer: HTMLDivElement;
+
+  onMount(async() => {
+    const app = await createPixiApp(divContainer.clientWidth, divContainer.clientHeight, divContainer)
+  })
+  
+</script>
+
+
+<div bind:this={divContainer} class="h-[100dvh] w-[100-dvw]">
+
+</div>
