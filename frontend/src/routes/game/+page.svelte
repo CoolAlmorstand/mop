ce9cd5f181a6c67e437be65f467804a64667bd03
Timothy Dexter <loinoobgaming33373@gmail.com>
2026-09-18 10:35:28 +0800

    connect renderer to page

--- frontend/src/routes/game/+page.svelte
+++ frontend/src/routes/game/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import { createPixiApp } from "$lib/pixi-js/init";
+  import { PixiRenderer } from "$lib/renderer/renderer";
   import { onMount } from "svelte";
 
   let divContainer: HTMLDivElement;
 
   onMount(async() => {
     const app = await createPixiApp(divContainer.clientWidth, divContainer.clientHeight, divContainer)
+    const renderer = new PixiRenderer(app)
   })
   
 </script>
