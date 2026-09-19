2d04ed4847a7c3c1b2c23bb177964ffefe690ea9
Timothy Dexter <loinoobgaming33373@gmail.com>
2026-09-20 05:29:42 +0800

    updat tilesTextureMap type

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -2,7 +2,7 @@ import type { Texture } from "pixi.js";
 import type { ITilesId } from "@mop/shared-types"
 
 
+export type tileOrientation = "NW" | "SE"
 
-
-export type ITilesTexureMap = Record<ITilesId, Texture>
+export type ITilesTexureMap = Record<ITilesId, Record<tileOrientation, Texture>>
 
