07cad2fa6421b4c6b71ca4cda4789bdbcb74c402
Timothy Dexter <loinoobgaming33373@gmail.com>
2026-09-21 23:25:45 +0800

    add base stats for entities

--- shared-types/src/index.ts
+++ shared-types/src/index.ts
@@ -7,7 +7,7 @@ export type IEntityType = "player" | "sheep"
 export type IEntity = {
   x: number;
   y: number;
-  hitSize: {width: number, height: number};
+  hitboxtSize: {width: number, height: number};
   health: number;
   speed: number;
   attack: number;
@@ -18,5 +18,31 @@ export type IEntity = {
 
 export interface IGameEvents {
   entitiesMove: (entities: IEntity[]) => void;
+}
+
+
+type entityStats = {
+  speed: number,
+  health: number,
+  attack: number,
+  hitboxWidth:number,
+  hitboxHeight: number
+}
+
+export const ENTITIES_BASE_STATS: Record<IEntityType, entityStats> = {
+  player: {
+    speed: 2,
+    health: 20,
+    attack: 5,
+    hitboxWidth:32,
+    hitboxHeight: 48
+  },
+  sheep: {
+    speed: 2,
+    health: 20,
+    attack: 5,
+    hitboxWidth:32,
+    hitboxHeight: 48
+  }
 }
 
