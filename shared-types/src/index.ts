14a15a0ff166c5577bb77f2236feab52e0a5da27
Timothy Dexter <loinoobgaming33373@gmail.com>
2026-09-21 21:50:34 +0800

    add entity type

--- shared-types/src/index.ts
+++ shared-types/src/index.ts
@@ -3,3 +3,14 @@
 export type ITilesId = "grass" | "sand"
 export type IEntityType = "player" | "sheep"
 
+
+export type IEntity = {
+  x: number;
+  y: number;
+  hitSize: {width: number, height: number};
+  health: number;
+  speed: number;
+  attack: number;
+  id: string;
+}
+
