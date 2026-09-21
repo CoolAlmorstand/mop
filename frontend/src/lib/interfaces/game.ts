deb1f0c40d7ec8961a277cb6bbbcf45f393a9393
Timothy Dexter <loinoobgaming33373@gmail.com>
2026-09-21 23:33:41 +0800

    add spawn entity type

--- frontend/src/lib/interfaces/game.ts
+++ frontend/src/lib/interfaces/game.ts
@@ -1,17 +1,16 @@
 
-import type { IGameEvents } from "@mop/shared-types"
+import type { IEntityType, IGameEvents } from "@mop/shared-types"
 import type EventEmmitter from "eventemitter3"
 
 
 export interface IGame {
   event: EventEmmitter<IGameEvents>;
+
+  movePlayer(x: number, y: number, id: string): void;
+
+  //spawn an entity and returns the id
+  spawnEntity(type: IEntityType, x: number, y: number): string;
 }
-
-
-
-
-
-
 
 
 
