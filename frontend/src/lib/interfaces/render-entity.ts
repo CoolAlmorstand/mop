43f924566afa9cbdaabc99ba791ed7d1e350bd57
Timothy Dexter <loinoobgaming33373@gmail.com>
2026-09-21 15:11:48 +0800

    added render-entity and entity renderer class

--- frontend/src/lib/interfaces/render-entity.ts
+++ frontend/src/lib/interfaces/render-entity.ts
@@ -5,10 +5,11 @@ import type { AnimatedSprite, Spritesheet } from "pixi.js";
 export interface IRenderEntity {
   //animation name, spriteSheet
   spriteSheets: Record<string, Spritesheet>;
+  currentSpriteSheet: string;
   animatedSprite: AnimatedSprite;
   id: string;
 
   playAnimation(name: string, speed: number, loop: boolean): void;
-  stopAnimation(name: string, speed: number, loop: boolean): void;
+  stopAnimation(name: string): void;
   changeSpriteSheet(name: string): void;
 }
